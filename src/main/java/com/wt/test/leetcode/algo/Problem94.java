ee0aecd6852a10424a5be12275e0a241e7fe5d92
吴添 <wb-wt780513@alibaba-inc.com>
2020-09-14 10:06:21 +0800

    94. 二叉树的中序遍历 by yipin 2020-09-14

---
+++ src/main/java/com/wt/test/leetcode/algo/Problem94.java
@@ -0,0 +1,44 @@
+package com.wt.test.leetcode.algo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 94. 二叉树的中序遍历
+ * https://leetcode-cn.com/problems/binary-tree-inorder-traversal/
+ *
+ * @author 一贫
+ * @date 2020/9/14
+ */
+public class Problem94 {
+    public static void main(String[] args) {
+
+    }
+
+    public static List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        traverse(root, result);
+        return result;
+    }
+
+    private static void traverse(TreeNode root, List<Integer> result) {
+        if (root == null)
+            return;
+        if (root.left != null)
+            traverse(root.left, result);
+        result.add(root.val);
+        if (root.right != null)
+            traverse(root.right, result);
+
+    }
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
