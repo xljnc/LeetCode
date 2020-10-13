a6338c94c7e3cb550093a2582a36eaf1add3cbfc
wb-wt780513@alibaba-inc.com <wb-wt780513@alibaba-inc.com>
2020-10-13 11:08:49 +0800

    783. 二叉搜索树节点最小距离 by yipin 2020-10-13

---
+++ src/main/java/com/wt/test/leetcode/algo/Problem783.java
@@ -0,0 +1,29 @@
+package com.wt.test.leetcode.algo;
+
+/**
+ * 783. 二叉搜索树节点最小距离
+ * https://leetcode-cn.com/problems/minimum-distance-between-bst-nodes/
+ *
+ * @author 一贫
+ * @date 2020/10/13
+ */
+public class Problem783 {
+    private int min = Integer.MAX_VALUE;
+
+    private int previous = Integer.MAX_VALUE;
+
+    public int minDiffInBST(TreeNode root) {
+        traverse(root);
+        return min;
+    }
+
+    public void traverse(TreeNode root) {
+        if (root == null)
+            return;
+        traverse(root.left);
+        int diff = Math.abs(root.val - previous);
+        min = Math.min(min, diff);
+        previous = root.val;
+        traverse(root.right);
+    }
+}
