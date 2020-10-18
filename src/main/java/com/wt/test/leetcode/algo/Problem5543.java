3f4f76411870e8ccfbb657e0498a7aac0fda0ec4
qiyu <jsj0804wt@126.com>
2020-10-18 11:03:17 +0800

    5543. 两个相同字符之间的最长子字符串  by qiyu 2020-10-18

---
+++ src/main/java/com/wt/test/leetcode/algo/Problem5543.java
@@ -0,0 +1,38 @@
+package com.wt.test.leetcode.algo;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 5543. 两个相同字符之间的最长子字符串
+ * https://leetcode-cn.com/contest/weekly-contest-211/problems/largest-substring-between-two-equal-characters/
+ *
+ * @author qiyu
+ */
+public class Problem5543 {
+
+    public int maxLengthBetweenEqualCharacters(String s) {
+        if (s.length() == 1)
+            return -1;
+        Set<Character> set = new HashSet<>();
+        int max = 0;
+        for (int i = 0; i < s.length() - 1; i++) {
+            char c = s.charAt(i);
+            if (set.contains(c))
+                continue;
+            for (int j = s.length() - 1; j > i; j--) {
+                if (s.charAt(j) == c) {
+                    max = Math.max(max, j - i - 1);
+                    set.add(c);
+                    break;
+                }
+            }
+        }
+        if (set.isEmpty())
+            return -1;
+        return max;
+    }
+
+
+}
