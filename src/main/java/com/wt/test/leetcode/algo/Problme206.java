111c728df7d5893f7a2df005ee77a26b1d58897e
wb-wt780513@alibaba-inc.com <wb-wt780513@alibaba-inc.com>
2020-10-20 19:12:11 +0800

    206. 反转链表 by yipin 2020-10-20

---
+++ src/main/java/com/wt/test/leetcode/algo/Problme206.java
@@ -0,0 +1,26 @@
+package com.wt.test.leetcode.algo;
+
+/**
+ * 206. 反转链表
+ * https://leetcode-cn.com/problems/reverse-linked-list/
+ *
+ * @author 一贫
+ * @date 2020/10/20
+ */
+public class Problme206 {
+
+    public ListNode reverseList(ListNode head) {
+        if (head == null)
+            return head;
+        ListNode pre = head;
+        ListNode curr = head.next;
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = pre;
+            pre = curr;
+            curr = next;
+        }
+        head.next = null;
+        return pre;
+    }
+}
