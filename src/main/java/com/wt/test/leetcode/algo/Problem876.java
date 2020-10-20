dad3aca1dc7050e9972926b14dafbd5fe6ed639d
wb-wt780513@alibaba-inc.com <wb-wt780513@alibaba-inc.com>
2020-10-20 20:00:38 +0800

    876. 链表的中间结点 by yipin 2020-10-20

---
+++ src/main/java/com/wt/test/leetcode/algo/Problem876.java
@@ -0,0 +1,26 @@
+package com.wt.test.leetcode.algo;
+
+/**
+ * 876. 链表的中间结点
+ * https://leetcode-cn.com/problems/middle-of-the-linked-list/
+ *
+ * @author 一贫
+ * @date 2020/10/20
+ */
+public class Problem876 {
+
+    public ListNode middleNode(ListNode head) {
+        if (head == null || head.next == null)
+            return head;
+        int count = 1;
+        ListNode root = head;
+        while ((root = root.next) != null) {
+            count++;
+        }
+        int step = count / 2;
+        while (step-- != 0) {
+            head = head.next;
+        }
+        return head;
+    }
+}
