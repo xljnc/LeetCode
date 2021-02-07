f02d7792853e90d040732dbec8377f6828d572ee
wb-wt780513@alibaba-inc.com <wb-wt780513@alibaba-inc.com>
2021-02-07 17:25:46 +0800

    剑指 Offer 64. 求1+2+…+n by yipin 2021-02-07

---
+++ src/main/java/com/wt/test/leetcode/lcof/Problem64.java
@@ -0,0 +1,18 @@
+package com.wt.test.leetcode.lcof;
+
+/**
+ * 剑指 Offer 64. 求1+2+…+n
+ * https://leetcode-cn.com/problems/qiu-12n-lcof/
+ *
+ * @author 一贫
+ * @date 2021/2/7
+ */
+public class Problem64 {
+
+    public int sumNums(int n) {
+        if (n == 1)
+            return 1;
+        return n + sumNums(n - 1);
+    }
+
+}
