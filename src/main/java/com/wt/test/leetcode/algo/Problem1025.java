6aee44030582cb7353c22af2115f43ff14558424
qiyu <jsj0804wt@126.com>
2021-03-01 16:59:45 +0800

    1025. 除数博弈 by yipin 2021-03-01

--- src/main/java/com/wt/test/leetcode/algo/Problem1025.java
+++ src/main/java/com/wt/test/leetcode/algo/Problem1025.java
@@ -10,6 +10,25 @@ package com.wt.test.leetcode.algo;
 public class Problem1025 {
 
     /**
+     * 思路：对每个数N，Alice的操作肯定会导致对手的数字为M(M<N)
+     * 因此只要判断是否存在一个数字M，导致对手必输即可。
+     */
+    public boolean divisorGame(int N) {
+        boolean[] result = new boolean[N + 2];
+        result[1] = false;
+        result[2] = true;
+        for (int i = 3; i <= N; i++) {
+            for (int j = 1; j < i; j++) {
+                if (i % j == 0 && !result[i - j]) {
+                    result[i] = true;
+                    break;
+                }
+            }
+        }
+        return result[N];
+    }
+
+    /**
      * 思路：根据Alice先手是基数还是偶数。
      * N = 1 时，没有选择，必输
      * N = 2 时，只能选1，对手数字为1，必胜
@@ -18,7 +37,7 @@ public class Problem1025 {
      * N为偶数时，可以选择因子是奇数还是偶数，所以步骤2会导致对手可选的数字肯定是奇数，
      * 最后对手可以选的数字最终为1，必胜
      */
-    public boolean divisorGame(int N) {
+    public boolean divisorGame1(int N) {
         return N % 2 == 0;
     }
 }
