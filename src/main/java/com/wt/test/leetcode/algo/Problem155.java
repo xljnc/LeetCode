44e98ec999f6100e1f757d3994f6d8d688079745
qiyu <jsj0804wt@126.com>
2021-04-07 11:51:42 +0800

    155. 最小栈 by yipin 2021-04-07

---
+++ src/main/java/com/wt/test/leetcode/algo/Problem155.java
@@ -0,0 +1,39 @@
+package com.wt.test.leetcode.algo;
+
+/**
+ * 155. 最小栈
+ * https://leetcode-cn.com/problems/min-stack/
+ *
+ * @author 一贫
+ * @date 2021/4/7
+ */
+public class Problem155 {
+
+
+     static class MinStack {
+
+          /**
+           * initialize your data structure here.
+           */
+          public MinStack() {
+
+          }
+
+          public void push(int val) {
+
+          }
+
+          public void pop() {
+
+          }
+
+          public int top() {
+
+          }
+
+          public int getMin() {
+
+          }
+     }
+
+}
