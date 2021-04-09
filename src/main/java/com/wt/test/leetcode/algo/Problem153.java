9ca030b147ee31901cca4e3c3705e501d57cd040
qiyu <jsj0804wt@126.com>
2021-04-09 18:18:39 +0800

    153. 寻找旋转排序数组中的最小值 by yipin 2021-04-08

--- src/main/java/com/wt/test/leetcode/algo/Problem153.java
+++ src/main/java/com/wt/test/leetcode/algo/Problem153.java
@@ -1,7 +1,5 @@
 package com.wt.test.leetcode.algo;
 
-import java.util.Arrays;
-
 /**
  * 153. 寻找旋转排序数组中的最小值
  * https://leetcode-cn.com/problems/find-minimum-in-rotated-sorted-array/
@@ -11,8 +9,27 @@ import java.util.Arrays;
  */
 public class Problem153 {
 
+    public static void main(String[] args) {
+        Problem153 problem153 = new Problem153();
+        int[] nums = {3, 4, 5, 1, 2};
+        int result = problem153.findMin(nums);
+        System.out.println(result);
+    }
+
     public int findMin(int[] nums) {
-        Arrays.sort(nums);
-        return nums[0];
+        int left = 0;
+        int right = nums.length - 1;
+        int pivot = nums[right];
+        int mid = (left + right) / 2;
+        while (mid > left && mid < right) {
+            if (nums[mid] <= pivot && nums[mid - 1] > pivot)
+                return nums[mid];
+            else if (nums[mid] > pivot)
+                left = mid;
+            else
+                right = mid;
+            mid = (left + right) / 2;
+        }
+        return Math.min(nums[mid], pivot);
     }
 }
