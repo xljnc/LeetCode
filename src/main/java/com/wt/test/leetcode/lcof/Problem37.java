360b72edbbfcdec34c3d494d61d8ec0cc3d3dc7d
qiyu <jsj0804wt@126.com>
2021-06-30 15:30:10 +0800

    剑指 Offer 37. 序列化二叉树 by yipin 2021-06-30

---
+++ src/main/java/com/wt/test/leetcode/lcof/Problem37.java
@@ -0,0 +1,90 @@
+package com.wt.test.leetcode.lcof;
+
+import com.wt.test.leetcode.algo.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 剑指 Offer 37. 序列化二叉树
+ * https://leetcode-cn.com/problems/xu-lie-hua-er-cha-shu-lcof/
+ *
+ * @author 一贫
+ * @date 2021/4/29
+ */
+public class Problem37 {
+
+    // Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
+        if (root == null)
+            return "[]";
+        List<String> result = new ArrayList<>();
+        List<TreeNode> row = new ArrayList<>(1);
+        row.add(root);
+        boolean flag = true;
+        while (row.size() != 0 && flag) {
+            flag = false;
+            List<TreeNode> nextRow = new ArrayList<>();
+            for (TreeNode node : row) {
+                if (node == null)
+                    result.add("null");
+                else {
+                    result.add(String.valueOf(node.val));
+                    nextRow.add(node.left);
+                    nextRow.add(node.right);
+                    if (node.left != null || node.right != null)
+                        flag = true;
+                }
+            }
+            row = nextRow;
+        }
+        int last = result.size() - 1;
+        for (int i = last; i >= 0; i--) {
+            if (!result.get(i).equals("null")) {
+                last = i;
+                break;
+            }
+        }
+        StringBuilder sb = new StringBuilder("[");
+        for (int i = 0; i <= last; i++) {
+            sb.append(result.get(i)).append(",");
+        }
+        sb.deleteCharAt(sb.length() - 1);
+        return sb.toString() + "]";
+    }
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+        if (data == null || data.length() == 0 || data.equals("[]"))
+            return null;
+        data = data.substring(1, data.length() - 1);
+        String[] datas = data.split(",");
+        Integer[] input = new Integer[datas.length];
+        for (int i = 0; i < datas.length; i++) {
+            input[i] = datas[i].equals("null") ? null : Integer.valueOf(datas[i]);
+        }
+        TreeNode root = new TreeNode(input[0]);
+        int index = 1;
+        List<TreeNode> row = new ArrayList<>(1);
+        row.add(root);
+        while (index < input.length) {
+            List<TreeNode> currRow = new ArrayList<>();
+            for (TreeNode node : row) {
+                if (node == null)
+                    continue;
+                if (index == input.length)
+                    break;
+                Integer currVal = input[index++];
+                node.left = currVal == null ? null : new TreeNode(currVal);
+                currRow.add(node.left);
+                if (index == input.length)
+                    break;
+                currVal = input[index++];
+                node.right = currVal == null ? null : new TreeNode(currVal);
+                currRow.add(node.right);
+            }
+            row = currRow;
+        }
+        return root;
+    }
+}
