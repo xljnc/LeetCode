aa5b255c140fd9c47e4269052e387c0dc0fc1878
qiyu <jsj0804wt@126.com>
2021-07-02 15:11:29 +0800

    1833. 雪糕的最大数量 by yipin 2021-07-02

--- src/main/java/com/wt/test/leetcode/algo/Problem1833.java
+++ src/main/java/com/wt/test/leetcode/algo/Problem1833.java
@@ -12,10 +12,31 @@ import java.util.Arrays;
 public class Problem1833 {
 
     public int maxIceCream(int[] costs, int coins) {
+        int[] bottles = new int[100001];
+        for (int v : costs) {
+            bottles[v]++;
+        }
+        int count = 0;
+        int index = 1;
+        while (index < 100001) {
+            if (coins < index)
+                break;
+            int div = coins / index;
+            int real = div > bottles[index] ? bottles[index] : div;
+            coins -= real * index;
+            count += real;
+            if (div < bottles[index])
+                break;
+            index++;
+        }
+        return count;
+    }
+
+    public int maxIceCream1(int[] costs, int coins) {
         Arrays.sort(costs);
         int count = 0;
         int index = 0;
-        while (coins > 0 && index < costs.length) {
+        while (index < costs.length) {
             if (coins < costs[index])
                 break;
             coins -= costs[index];
