ca56e2ddc02b503ce0f3f6bf4c67a96651f103ea
qiyu <jsj0804wt@126.com>
2021-07-08 10:00:24 +0800

    1. 两数之和 by yipin 2021-07-08

--- src/main/java/com/wt/test/leetcode/algo/Problem1.java
+++ src/main/java/com/wt/test/leetcode/algo/Problem1.java
@@ -14,11 +14,26 @@ public class Problem1 {
     public static void main(String[] args) {
         int[] nums = {2, 7, 11, 15};
         int target = 9;
-        int[] result = twoSum(nums,target);
+        int[] result = twoSum(nums, target);
         System.out.println(result);
     }
 
     public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> numMap = new HashMap<>();
+        int[] result = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            int left = target - nums[i];
+            if (numMap.containsKey(left)) {
+                result[0] = numMap.get(left);
+                result[1] = i;
+                return result;
+            }
+            numMap.put(nums[i], i);
+        }
+        return result;
+    }
+
+    public static int[] twoSum1(int[] nums, int target) {
         Map<Integer, Integer> numMap = new HashMap<>();
         int[] result = new int[2];
         for (int i = 0; i < nums.length; i++) {
