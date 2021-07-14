77934df67673982984f049cd869120dcb6989c3d
qiyu <jsj0804wt@126.com>
2021-07-14 17:23:14 +0800

    83. 删除排序链表中的重复元素 by yipin 2021-07-14

--- src/main/java/com/wt/test/leetcode/algo/Problem83.java
+++ src/main/java/com/wt/test/leetcode/algo/Problem83.java
@@ -23,22 +23,4 @@ public class Problem83 {
         return result;
     }
 
-
-    private static class ListNode {
-        int val;
-        ListNode next;
-
-        ListNode() {
-        }
-
-        ListNode(int val) {
-            this.val = val;
-        }
-
-        ListNode(int val, ListNode next) {
-            this.val = val;
-            this.next = next;
-        }
-    }
-
 }
