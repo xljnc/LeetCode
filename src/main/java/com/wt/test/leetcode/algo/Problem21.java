c68562d7531b8c63737dd35a23d9279ec8bcac51
qiyu <jsj0804wt@126.com>
2021-07-14 16:55:08 +0800

    21. 合并两个有序链表 by yipin 2021-07-14

--- src/main/java/com/wt/test/leetcode/algo/Problem21.java
+++ src/main/java/com/wt/test/leetcode/algo/Problem21.java
@@ -42,21 +42,4 @@ public class Problem21 {
             curr.next = l2;
         return result.next;
     }
-
-    private static class ListNode {
-        int val;
-        ListNode next;
-
-        ListNode() {
-        }
-
-        ListNode(int val) {
-            this.val = val;
-        }
-
-        ListNode(int val, ListNode next) {
-            this.val = val;
-            this.next = next;
-        }
-    }
 }
