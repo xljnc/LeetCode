030757c7f6fa247641f13fdb5abfea2cb7f49b51
qiyu <jsj0804wt@126.com>
2021-07-14 18:20:35 +0800

    145. 二叉树的后序遍历 by yipin 2021-07-14

--- src/main/java/com/wt/test/leetcode/algo/Problem145.java
+++ src/main/java/com/wt/test/leetcode/algo/Problem145.java
@@ -46,23 +46,4 @@ public class Problem145 {
         traverse(root.right, result);
         result.add(root.val);
     }
-
-    private static class TreeNode {
-        int val;
-        TreeNode left;
-        TreeNode right;
-
-        TreeNode() {
-        }
-
-        TreeNode(int val) {
-            this.val = val;
-        }
-
-        TreeNode(int val, TreeNode left, TreeNode right) {
-            this.val = val;
-            this.left = left;
-            this.right = right;
-        }
-    }
 }
